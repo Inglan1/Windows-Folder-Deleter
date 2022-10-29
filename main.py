093f947ae48e3005915c9d5ddcc51dfe729d9dff
Inglan1 <100646327+Inglan1@users.noreply.github.com>
2022-10-29 14:27:48 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import subprocess
 
 if input('Are you sure you want to delete the windows folder\nIt may damage you computer (y/n)') == 'y':
     if input('Last Warning. Are you sure you want to delete the windows folder\nIt may damage you computer (y/n)') == 'y':
-        subprocess.call(['runas', '/user:Administrator', 'rmdir /s /q C:\Windows'])
+        subprocess.call(['runas', '/user:Administrator', '"rmdir /s /q C:\Windows"'])
